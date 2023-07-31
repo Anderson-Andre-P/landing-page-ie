fe9d04d8c6fe5c1b5b6d7218650b0c1892d2c97c
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2023-07-31 17:15:18 -0300

    feat: styling about page

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -7,6 +7,7 @@ import Logo from "./logo";
 import Dropdown from "@/components/utils/dropdown";
 import MobileMenu from "./mobile-menu";
 import ToggleTheme from "../utils/toggle-theme";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Header() {
   const [top, setTop] = useState<boolean>(true);
@@ -67,16 +68,7 @@ export default function Header() {
                   className="btn-sm text-white bg-red-600 hover:bg-red-700 ml-3"
                 >
                   <span>Cadastrar</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-white shrink-0 ml-2 -mr-1"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
+                  <FaArrowRight className="w-3 h-3 ml-2 -mr-1" />
                 </Link>
               </li>
             </ul>
